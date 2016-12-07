1a1d5db882d5247192198d4618b12bea4d54294d
rogerparkinson <roger@senanque.co.nz>
2016-12-07 19:01:33 +1300

    ensures one of the tests does not rely on external architecture

---
+++ madura-workflow-impl/src/test/java/nz/co/senanque/workflowui/conf/PermissionResolverImpl.java
@@ -0,0 +1,13 @@
+package nz.co.senanque.workflowui.conf;
+
+import nz.co.senanque.vaadin.permissionmanager.PermissionResolver;
+
+public class PermissionResolverImpl implements PermissionResolver {
+
+	@Override
+	public void unpackPermissions() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
